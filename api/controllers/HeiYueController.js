bc0500eb96c9e3e5c00d8310ccbe78786814de5f
ojlovecd <lonelyjano@163.com>
2017-06-15 22:23:53 +0800

    addHYUser

--- api/controllers/HeiYueController.js
+++ api/controllers/HeiYueController.js
@@ -86,7 +86,7 @@ module.exports = {
 			res.send({result: 'error: no openid!'});
 		}
 		else {
-			HYUser.findOne({where: {openid: openid}}).exec(function(err, u){
+			HYUser.findOne({where: {openid: user.openid}}).exec(function(err, u){
 				if(err){
 					res.send(err);
 				}
